9a8b24797a7d7af078e7d65aad675cbd3e4d43fd
Victor Paranhos <victor_paranhos@hotmail.com>
2024-01-08 11:43:10 -0300

    price add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const [slideRef] = useKeenSlider({
     slides: {
       perView: 3,
-      spacing: 30,
+      spacing: 48,
     }
   })
 
@@ -21,15 +21,31 @@ export default function Home() {
     <HomeContainer ref={slideRef} className="keen-slider">
       <Product className="keen-slider__slide number-slide1">
         <Image width={520} height={480} src={camisa2.src} alt="" />
+        <div>
+          <strong>Camisa Beyond the Limits</strong>
+          <span>R$ 49,90</span>
+        </div>
       </Product>
       <Product className="keen-slider__slide number-slide2">
         <Image width={520} height={480} src={camisa3.src} alt="" />
+        <div>
+          <strong>Camisa Beyond the Limits</strong>
+          <span>R$ 49,90</span>
+        </div>
       </Product>
       <Product className="keen-slider__slide number-slide3">
         <Image width={520} height={480} src={camisa4.src} alt="" />
+        <div>
+          <strong>Camisa Beyond the Limits</strong>
+          <span>R$ 49,90</span>
+        </div>
       </Product>
       <Product className="keen-slider__slide number-slide4">
         <Image width={520} height={480} src={camisa5.src} alt="" />
+        <div>
+          <strong>Camisa Beyond the Limits</strong>
+          <span>R$ 49,90</span>
+        </div>
       </Product>   
     </HomeContainer>  
   )
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,13 +1,58 @@
 import { styled } from '..';
 
 export const HomeContainer= styled('main', {
-    width: '100%',
+
     minHeight: '65.6rem',
     maxWidth: 'calc(100vw - ((100vw - 118rem)/2))',
     marginLeft: 'auto',
-    display: 'flex'
+
 })
 
 export const Product= styled('a', {
     background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+    width: '69.6rem',
+    position: 'relative',
+    borderRadius: 8,
+    cursor: 'pointer',
+
+
+
+    img: {
+        objectFit: 'cover',
+        width: '100%',
+    },
+
+    '&:hover div': {
+        transform: 'translateY(0%)',
+        opacity: 1,
+    },
+
+    div: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        backgroundColor: '$gray800',
+        padding: '3.2rem 4rem 3.2rem',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+        transform: 'translateY(120%)',
+        transition: 'transform 0.4s, opacity 0.4s',
+        opacity: 0,
+        borderRadius: 6,
+
+       
+
+        strong: {
+            fontSize: '2rem'
+        },
+
+        span: {
+            color: '$green300',
+            fontWeight: 'bold',
+            fontSize: '2.4rem'
+        }
+    }
 })
