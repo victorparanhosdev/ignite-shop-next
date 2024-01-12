97130c2deb9e92185599f09c14eb21fd02accc4f
Victor Paranhos <victor_paranhos@hotmail.com>
2024-01-12 00:01:33 -0300

    Update [id].tsx

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -9,12 +9,10 @@ import { stripe } from "@/lib/stripe";
 import Stripe from "stripe";
 import { useRouter } from "next/router";
 
-
 import Head from "next/head";
 import { useCart } from "@/hooks/useCart";
 import { toast } from "react-toastify";
 import { Loading } from "@/components/loading";
-
 
 interface ProductProps {
   product: {
@@ -22,42 +20,36 @@ interface ProductProps {
     name: string;
     imageUrl: string;
     price: string;
-    description: string,
-    defaultPriceId: string,
-  }
+    description: string;
+    defaultPriceId: string;
+  };
 }
 
 export default function Product({ product }: ProductProps) {
-  const {dataCart, setdataCart} = useCart()
+  const { dataCart, setdataCart } = useCart();
 
+  function handleAddProduct() {
+    const isProductInCart = dataCart.some((item) => item.id === product.id);
 
+    if (!isProductInCart) {
+      setdataCart((prevState) => [{ ...product, quantity: 1 }, ...prevState]);
+    } else {
+      setdataCart((prevState) =>
+        prevState.map((data) => {
+          if (data.id === product.id) {
+            return { ...data, quantity: data.quantity + 1 };
+          }
 
-  function handleAddProduct(){
+          return data;
+        })
+      );
+    }
 
-
-  const isProductInCart = dataCart.some(item => item.id === product.id);
-
-  if (!isProductInCart) {
-    setdataCart(prevState => [{...product, quantity: 1}, ...prevState]);
-  } else {
-    setdataCart(prevState => prevState.map(data => {
-      if(data.id === product.id){
-        return {...data, quantity: data.quantity + 1}
-      }
-
-      return data
-    }));
-  }
-
-  toast.success(`${product.name} adicionado(a)`, {
-    theme: 'dark',
-    autoClose: 500,
-    pauseOnHover: false,
-  })
-
-
-    
-
+    toast.success(`${product.name} adicionado(a)`, {
+      theme: "dark",
+      autoClose: 500,
+      pauseOnHover: false,
+    });
   }
   const { isFallback } = useRouter();
 
@@ -67,9 +59,9 @@ export default function Product({ product }: ProductProps) {
 
   return (
     <>
-    <Head>
-      <title>{`${product.name} | Ignite Shop`}</title>
-    </Head>
+      <Head>
+        <title>{`${product.name} | Ignite Shop`}</title>
+      </Head>
       <ProductHome>
         <ImageContainer>
           <Image width={520} height={480} src={product.imageUrl} alt="" />
@@ -88,13 +80,27 @@ export default function Product({ product }: ProductProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const product = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  const arrayList = product.data.map(item=> {
+    return {
+      params: {
+        id: item.id
+      }
+    }
+  })
+  console.log(arrayList)
+
   return {
-    paths: [{ params: { id: "prod_PL4Ew0t5dY81jk" } }],
+    paths: arrayList,
     fallback: true,
   };
 };
 
-export const getStaticProps: GetStaticProps<any, { id: string }> = async ({params}) => {
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
   const productID = params!.id;
 
   const product = await stripe.products.retrieve(productID, {
@@ -114,7 +120,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({param
           currency: "BRL",
         }).format(price.unit_amount! / 100),
         description: product.description,
-        defaultPriceId: price.id
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60,
